0603e3e227321174dc2794b16b33aa7a0e09ab31
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-06-07 10:20:06 +0530

    push using terminal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat,
+        repellendus, neque tempora magnam natus necessitatibus dolore explicabo
+        autem quod, quaerat debitis vitae hic aut voluptatum reprehenderit in
+        ipsa mollitia. Perspiciatis.
+      </p>
+    </>
+  );
+}
+
+export default App;
